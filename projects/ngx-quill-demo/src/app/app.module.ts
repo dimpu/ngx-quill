f044ab833389a96a925e31a45ed80d46581f0226
Dimpu <buddhaaravind@gmail.com>
2018-10-21 13:51:45 -0400

    update demo

--- projects/ngx-quill-demo/src/app/app.module.ts
+++ projects/ngx-quill-demo/src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { NgxQuillModule } from '@dimpu/ngx-quill';
+import { NgxQuillModule } from '@dimpu/ngx-quill';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    NgxQuillModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [HomeComponent]
 })
 export class AppModule { }
