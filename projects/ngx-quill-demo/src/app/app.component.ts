ba7b07ba83e5b69a86180f359ec45e653d216f20
Dimpu <buddhaaravind@gmail.com>
2018-10-28 17:03:00 -0400

    feature/mm-server

--- projects/ngx-quill-demo/src/app/app.component.ts
+++ projects/ngx-quill-demo/src/app/app.component.ts
@@ -1,80 +1,8 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
-declare const require: any;
-const Quill = require('quill');
-
-import { ImageDrop } from 'quill-image-drop-module';
-import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
-Quill.register('modules/imageDrop', ImageDrop);
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
-  public form: FormGroup;
-  public content: AbstractControl;
-  public quill: any;
-
-  editorOptions = {
-    modules: {
-      formula: true,
-      imageDrop: true,
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        ['formula'],
-        ['image', 'code-block'],
-        [{ 'header': 1 }, { 'header': 2 }],
-      ],
-    }
-  };
-  editorOptions2 = {
-    theme: 'bubble',
-    modules: {
-      formula: true,
-      imageDrop: true,
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        ['formula'],
-        ['image', 'code-block'],
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-      ],
-      // ImageResize: {}
-    }
-  };
-
-  constructor(private _fb: FormBuilder) {
-    this.form = _fb.group({
-      'content': ['<p>Im inline editor exmaple </p>', Validators.compose([Validators.required])],
-    });
-
-    this.content = this.form.controls['content'];
-  }
-
-  onContentChanged($event) {
-    console.log($event);
-  }
-
-  onReady($event) {
-    this.quill = $event;
-    const customButton = document.querySelector('#custom-button');
-    customButton.addEventListener('click', function() {
-      const range = this.quill.getSelection();
-      if (range) {
-        this.quill.insertText(range.index, 'Ω');
-      }
-    });
-  }
-
-  public submitAnnouncement(values: Object): void {
-    if (this.form.valid) {
-       console.log('Submit!', values);
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.quill.disable();
-  }
-}
+export class AppComponent { }
--- projects/ngx-quill-demo/src/app/app.module.ts
+++ projects/ngx-quill-demo/src/app/app.module.ts
@@ -4,22 +4,51 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgxQuillModule } from '@dimpu/ngx-quill';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatDialogModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { CustomToolbarComponent } from './custom-toolbar/custom-toolbar.component';
+import { ThemesComponent } from './themes/themes.component';
+import { ImageHandlerComponent } from './image-handler/image-handler.component';
+import { ImageUploaderComponent } from './modals/image-uploader/image-uploader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    CustomToolbarComponent,
+    ThemesComponent,
+    ImageHandlerComponent,
+    ImageUploaderComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    LayoutModule,
+
     NgxQuillModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: []
+  exports: [],
+  entryComponents: [ImageUploaderComponent]
 })
 export class AppModule { }
