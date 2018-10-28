ec24b6d66c13688ea423b87010c3706fd49da874
Dimpu <buddhaaravind@gmail.com>
2018-10-28 17:11:04 -0400

    WIP

---
+++ projects/ngx-quill-demo/src/app/modals/image-uploader/image-uploader.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-uploader',
+  templateUrl: './image-uploader.component.html',
+  styleUrls: ['./image-uploader.component.scss']
+})
+export class ImageUploaderComponent implements AfterViewInit {
+  apiConfig: any = {
+    baseUrl: 'http://localhost:3100/api/',
+    listUrl: 'list',
+    uploadUrl: 'upload'
+  };
+
+  constructor() { }
+
+  onFileUploaded(res) {
+    console.log(res);
+  }
+
+  onFileSelected($event) {
+    console.log($event);
+  }
+
+  ngAfterViewInit(): void { }
+
+}
