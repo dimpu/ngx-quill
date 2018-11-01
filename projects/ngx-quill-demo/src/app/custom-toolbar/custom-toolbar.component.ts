4576f8bf96677e4270fd735c06b7f9d5e3728da4
Dimpu <buddhaaravind@gmail.com>
2018-11-01 13:53:16 -0400

    update to v7.0.7

--- projects/ngx-quill-demo/src/app/custom-toolbar/custom-toolbar.component.ts
+++ projects/ngx-quill-demo/src/app/custom-toolbar/custom-toolbar.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+// import * as QuillNS from 'quill';
+// import { Quill } from 'quill';
+let Quill: any = (<any>window).Quill;
+declare var require: any;
+
+if (!Quill) {
+  Quill = require('quill');
+}
+
+const BlockEmbed: any = Quill.import('blots/block/embed');
+class OIBlot extends BlockEmbed {
+
+
+}
+OIBlot.blotName = 'OI';
+OIBlot.tagName = 'order-interation';
+Quill.register(OIBlot);
 
 @Component({
   selector: 'app-custom-toolbar',
@@ -21,6 +38,8 @@ export class CustomToolbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(Quill);
+
   }
 
   onReady(quill) {
@@ -34,9 +53,15 @@ export class CustomToolbarComponent implements OnInit {
     });
     const customButton2 = document.querySelector('#custom-button2');
     customButton2.addEventListener('click', function() {
-      const range = quill.getSelection();
+      const range = quill.getSelection(true);
       if (range) {
-        quill.insertText(range.index, '<order-interation></order-interation>');
+        // quill.insertEmbed(range.index, '<order-interation></order-interation>');
+        quill.insertText(range.index, '\n', Quill.sources.USER);
+        const name = 'samc-name-1';
+        quill.insertEmbed(range.index + 1, 'order-interation', name, Quill.sources.USER);
+        quill.formatText(range.index + 1, 1, { height: '170', width: '400' });
+        quill.setSelection(range.index + 2, Quill.sources.SILENT);
+
       }
     });
   }
